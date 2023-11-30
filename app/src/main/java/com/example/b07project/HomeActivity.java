9044a3145094c74caf520eb6d39bc3eda58260dc
Andrew Tam <andrew360tam@gmail.com>
2023-11-30 09:45:24 -0500

    added ui for navigation and changed sign in and sign up ui

--- app/src/main/java/com/example/b07project/HomeActivity.java
+++ app/src/main/java/com/example/b07project/HomeActivity.java
@@ -3,9 +3,13 @@ package com.example.b07project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,20 +33,37 @@ public class HomeActivity extends AppCompatActivity {
         username = getIntent().getStringExtra("username");
         isAdmin = getIntent().getBooleanExtra("isAdmin", false);
         db = FirebaseDatabase.getInstance(getString(R.string.database_link)).getReference();
-    }
 
-    public void onClickHello(View helloButton) {
-        DateTime dt = new DateTime(2023, 11, 21, 22, 9);
-        db.child("Time").setValue(dt);
-        db.child("Time").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        //profile button with pop-up menu for POST checker and sign out
+        Button profileButton = findViewById(R.id.profileButton);
+        profileButton.setText(username.substring(0, 1).toUpperCase());
+        profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DateTime dt = task.getResult().getValue(DateTime.class);
-                    Log.d("testDateTime", dt.getYear() + " " + dt.getMonth() + " " + dt.getDay() + " " + dt.getHour() + " " + dt.getMinute());
-                }
+            public void onClick(View view) {
+                showPopupMenu(view);
             }
         });
+    }
+
+    public void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.postCheckerButton) {
+                    //TO DO
+                    return true;
+                } else if (itemId == R.id.signOutButton) {
+                    Intent intent = new Intent(HomeActivity.this, SignInActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 
 }
